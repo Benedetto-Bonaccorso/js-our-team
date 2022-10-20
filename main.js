c8dd46ddb3da454e8e866962e26e6a87b0af2c03
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2022-10-20 12:54:16 +0200

    milestone 1 e 27

---
+++ main.js
@@ -0,0 +1,28 @@
+let users = [
+
+    {Name: "Wayne Barnett",
+    Role: "Founder & CEO",
+    Image: "wayne-barnett-founder-ceo.jpg"}, 
+
+    {Name: "Angela",
+    Role: "Chief Editor",
+    Image: "angela-caroll-chief-editor.jpg"}, 
+
+    {Name: "Waleter gordon",
+    Role: "Chief Editor",
+    Image: "walter-gordon-office-manager.jpg"}, 
+
+    {Name: "Waleter gordon",
+    Role: "Social Media Manager",
+    Image: "angela-lopez-social-media-manager.jpg"},
+
+    {Name: "Scott Estrada",
+    Role: "Developer",
+    Image: "scott-estrada-developer.jpg"},
+
+    {Name: "Barbara Ramos",
+    Role: "Grafic designer",
+    Image: "barbara-ramos-graphic-designer.jpg"},
+]
+
+console.log(users)
